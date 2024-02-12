b3b043216f23ec792f3eefe5fc01d8819afb715d
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-12 22:52:03 +0530

    First Commit

---
+++ WebApp_LaptopMVC/Models/Laptop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_LaptopMVC.Models
+{
+    [Table("Laptop")]
+    public class Laptop
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price {  get; set; }
+        public string RAM {  get; set; }
+        public string OS { get; set; }
+        public string CPU { get; set; }
+
+    }
+}
